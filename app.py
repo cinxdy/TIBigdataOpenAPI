6d43239f09fb879c80a09f2773be237ee8a6488a
cinxdy <cinxdys2@gmail.com>
2020-10-16 06:31:47 +0000

    update app.py and mongotest.py

--- app.py
+++ app.py
@@ -1,34 +1,32 @@
 from flask import Flask, jsonify, request, Response, render_template
 from flask_restful import Resource, Api
 from elasticsearch import Elasticsearch
-from secrets import token_urlsafe 
+from secrets import token_urlsafe
 from passlib.hash import pbkdf2_sha512
 import json
+from mongotest import *
 
 app = Flask(__name__)
 
-#import socket
-#def get_ip_address():
-#    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#    s.connect(("8.8.8.8", 80))
-#    return s.getsockname()[0]
-
-#serverUrl = get_ip_address()  # '192.168.0.110'
-#if(serverUrl != "http://203.252.112.15:9200"):
-#    serverUrl="http://localhost:9200"
-#else:
-#serverUrl = "203.252.112.14"
-#print(serverUrl)
-
-# ElasticSearch connection
-#es = Elasticsearch(hosts=serverUrl, port=9200)
-#print(es)
-#print (es.cat.indices())
-#index_name='nkdb200914'
-
 @app.route('/')
+@app.route('/mainPage')
 def index():
-    return 'Welcome to Kubic!'
+    return render_template('mainPage.html')
+
+@app.route('/myInform')
+def myInform():
+    return render_template('myInform.html',doc=getInform())
+
+#@app.route('/Register')
+#def index():
+#    return render_template('Register.html')
+#
+#    
+#@app.route('/Management')
+#def index():
+#    return render_template('Management.html')
+
+
 
 key = token_urlsafe(16)
 hashKey = pbkdf2_sha512.hash(key)
@@ -42,12 +40,9 @@ def api():
     title = request.args.get('title',"")
     body = request.args.get('body',"")
     
-    #word = '문재인'
-    #res = es.search(index=index_name, body={"query": {"match":{"post_body":word}}})
-    #return render_template('main.html', response=res)
     if pbkdf2_sha512.verify(secretKey, hashKey):
         return "search for:"+title +"-"+ body
     return "cannot login"
 
 if __name__== "__main__":
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='127.0.0.1',debug=True)
--- mongotest.py
+++ mongotest.py
@@ -1,26 +1,34 @@
 
 from pymongo import MongoClient
-from bson.objectid import ObjectId
-
 from secrets import token_urlsafe 
 from passlib.hash import pbkdf2_sha512
 import datetime
 
 client = MongoClient('localhost',27017)
-userdb = client["user"]
+db = client.user
 
-def makeCode():    
+email_logined = "cindy@handong.edu"
+
+def makeCode():
     key = token_urlsafe(16)
     hashKey = pbkdf2_sha512.hash(key)
     return hashKey
 
-def addApiUser(email):
+def login(email,password):
+    doc = db.users.find_one({"email": email})
+    #print(doc)
+    if doc != None and password == doc['password']:
+        email_logined = email
+        return "Login Succeeded"
+    return "Login Failed"
+
+def register():
     now = datetime.datetime.now().date()
 
     post = {
         "app_name" : input("app_name>"),
         "app_purpose" : input("app_purpose>"),
-        "user_email" : email,
+        "user_email" : email_logined,
         "veri_code" : makeCode(),
         "reporting_date" : {
             'year': int(now.strftime('%y')),
@@ -36,4 +44,15 @@ def addApiUser(email):
     }
 
     print(post)
-    userdb.apiUser.insert_one(post)
+    db.apiUser.insert_one(post)
+
+def getInform():
+    doc = db.apiUser.find({"email": email_logined})
+    return doc
+
+if __name__ == "__main__":
+    email_try = input()
+    password_try = input()
+    
+    print(login(email_try,password_try))
+    print(getInform())
