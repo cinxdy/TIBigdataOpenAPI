c0d089b623233e1504d61cbf005b0d104e29965a
cinxdy <cinxdys2@gmail.com>
2021-01-08 17:42:16 +0900

    ES account updated

--- estest.py
+++ estest.py
@@ -1,9 +1,10 @@
 from elasticsearch import Elasticsearch
+import esAccount as esAcc
 
 def esSearch(request, host ='203.252.103.104', index='nkdb200803'):
     #Connect to DB
-    #host = '203.252.112.15'
-    ES = Elasticsearch(hosts=host)
+    host = '203.252.112.14'
+    ES = Elasticsearch([{'host': host, 'port': '9200'}], http_auth=(esAcc.id, esAcc.password)
     
     #search the document
     #res = ES.search(index=index)
@@ -19,3 +20,5 @@ def esSearch(request, host ='203.252.103.104', index='nkdb200803'):
     })
     print(res)
     return res
+host = '203.252.112.14'
+print(Elasticsearch([{'host': host, 'port': '9200'}], http_auth=('elastic', 'epp2020')).cat.indices())
